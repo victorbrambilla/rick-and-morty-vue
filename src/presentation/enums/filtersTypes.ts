bbaf944b0a1783b727bfddb4d676cc01a754a0a8
victor <victor-brambilla@hotmail.com>
2022-08-30 13:35:10 -0400

    feat: Add enums

---
+++ src/presentation/enums/filtersTypes.ts
@@ -0,0 +1,7 @@
+export const FilterTypes = [
+  { value: 'nenhum', label: 'Nenhum' },
+  { value: 'status', label: 'Status' },
+  { value: 'especie', label: 'Espécie' },
+  { value: 'genero', label: 'Gênero' },
+  { value: 'nome', label: 'Nome' },
+]
