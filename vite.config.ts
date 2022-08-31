165674d1c678f0adc22917359ed57cf2db37d010
victor <victor-brambilla@hotmail.com>
2022-08-31 16:03:19 -0400

    chore: Add quasar

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+
+    quasar(),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
